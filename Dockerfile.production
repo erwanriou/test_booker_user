f76bda078ab714493535fc165aaf8f7908f23022
erwanriou <rr.1.riou@gmail.com>
2021-09-27 23:31:55 +0200

    [Project] - update the common library

--- Dockerfile.production
+++ Dockerfile.production
@@ -4,7 +4,7 @@ WORKDIR /app
 COPY package.json .
 COPY .env.local .
 
-RUN echo -e "registry=https://registry.npmjs.org \ntimeout=60000" > .npmrc
+RUN echo -e "@erwanriou:registry=https://npm.pkg.github.com/erwanriou \nregistry=https://registry.npmjs.org \ntimeout=60000" > .npmrc
 RUN rm .env.local
 RUN npm install --only=prod
 COPY . .
--- scripts/env.sh
+++ scripts/env.sh
@@ -2,7 +2,6 @@
 if
   [ ! -f .npmrc ]
 then
-  # CREATE NEEDED RESSOURCES
-  echo "registry=https://registry.npmjs.org" > .npmrc
+  echo "@erwanriou:registry=https://npm.pkg.github.com/erwanriou \nregistry=https://registry.npmjs.org" > .npmrc
   echo "semi: false \ntrailingComma: \"none\" \narrowParens: \"avoid\" \nprintWidth: 150" > .prettierrc.yml
 fi
